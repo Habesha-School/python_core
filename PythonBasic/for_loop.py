c9c205875ed2b2f4f1d05295b67579a9f9131055
HABESHASCHOOL <habeshaschool@gmail.com>
2020-06-18 21:48:20 -0400

    print even number format has been changed

--- PythonBasic/for_loop.py
+++ PythonBasic/for_loop.py
@@ -28,7 +28,7 @@ for i in range(max_number + 1):
 
 # print sum of odd and even numbers up to the given number
 print("*"*30)
-print("Sum of even numbers up to "+str(max_number)+" is " + str(sum_of_even_number))
+print("Sum of even numbers up to {} is {}".format(max_number, sum_of_even_number))
 print("Sum of odd numbers up to {} is {}".format(max_number, sum_of_odd_numbers))
 print("sum of numbers up to {} is {}".format(max_number, sum_of_all_numbers))
 
