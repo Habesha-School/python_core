31e000afdb1cb37463096b5b2fe9eb366fa4d7af
HABESHASCHOOL <habeshaschool@gmail.com>
2020-06-19 14:39:06 -0400

    touble in java has been completed

---
+++ PythonBasic/collection_touple.py
@@ -0,0 +1,11 @@
+
+"""" touple is a non immutable ( not modifiable) collection """
+
+student_names = ("Jay", "Eshetie", "Jyanti", "Hareg",
+                 "hiber", "Amsalu", "tigistu", "Amsalu")
+print(student_names[5])
+print(student_names[5:])
+number_of_students = len(student_names)
+print("total number of studetns {}".format(number_of_students))
+print(student_names.count("Amsalu"))
+print(student_names)
